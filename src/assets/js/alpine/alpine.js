2558b07b9bb7f066e943c8641d600f20092903fc
Dominic <dominic.kirste@gmail.com>
2022-09-01 22:25:42 +0200

    created alpine store architecture

--- src/assets/js/alpine/alpine.js
+++ src/assets/js/alpine/alpine.js
@@ -1,6 +1,9 @@
 import Alpine from 'alpinejs';
 import testimonials from './data/testimonials.js';
+import counter from './store/counter.store.js';
 
 Alpine.data('testimonials', testimonials);
+
+Alpine.store('counter', counter());
 
 Alpine.start();
---
+++ src/assets/js/alpine/store/counter.store.js
@@ -0,0 +1,5 @@
+import jsonURL from '/assets/json/clients.json?url';
+export default () => ({
+  count: 0,
+  url: jsonURL,
+});
